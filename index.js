bba90b10b466f9331f167e579477f443da6935e3
Bartosz Szura <bartosz.szura100@gmail.com>
2019-09-04 23:07:20 +0200

    Hello World

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(`<html>
+<head>
+  
+</head>
+
+<body>
+HEllo World
+</body>
+
+</html> `))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
