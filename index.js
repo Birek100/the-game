edc949779aad119f3e02507659ee61988a73614c
Bartosz Szura <bartosz.szura100@gmail.com>
2019-09-14 17:09:21 +0200

    add background file and update index.js

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+app.use('/static', express.static('public'));
 app.get('/', (req, res) => res.send(`<html>
 <head>
+<link rel="stylesheet" type="text/css" href="/static/styles.css">
+<title>Penguin Game</title>
 </head>
 <body>
-Hello World
+<header>
+<h1>Pinguins Game</h1>
+</header>
+<div class="container">
+<section>
+<form action="#" method="post" autocomplete="off">
+<div class="register">
+<label for="name">Name</label><br>
+<input id="name" type="text" required>
+</div>
+<div class="register">
+<label for="password">Password</label><br>
+<input id="password" type="password" required>
+</div>
+<div class="register">
+<input type="submit" value="Play">
+</div>
+</section>
+<footer>
+All rights reserved &copy
+</footer>
+
+</div>
 </body>
 </html> `))
 
