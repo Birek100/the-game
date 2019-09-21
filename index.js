376db91960ea981f3d5c88a4b79617f6b70c2968
Bartosz Szura <bartosz.szura100@gmail.com>
2019-09-21 14:49:06 +0200

    update index.js

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const port = 3000
 const fs = require('fs');
 app.use('/static', express.static('public'));
 
-const data = fs.readFileSync('./views/login.html','utf8' ,(err, data) => {
-  if (err) throw err;
-});
+const data = fs.readFileSync('./views/login.html','utf8'); 
 
 app.get('/', (req, res) => res.send(data))
 
