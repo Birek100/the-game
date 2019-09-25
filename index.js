53f333a3a40db5aefbb24043d6f5dafbe0ff32fa
Bartosz Szura <bartosz.szura100@gmail.com>
2019-09-25 08:19:42 +0200

    udpate index.js and add new file game.html

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ const data = fs.readFileSync('./views/login.html','utf8');
 
 app.get('/', (req, res) => res.send(data))
 
+const data1 = fs.readFileSync('./views/game.html','utf8');
+
+app.get('/game', (req, res) => res.send(data1))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
