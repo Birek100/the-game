fa79fe4dace9a6e37eafdccc3992e1b2dfe5231d
Birek100 <48009602+Birek100@users.noreply.github.com>
2019-09-25 18:40:40 +0200

    GAME-14 - Create new Express.js route /game with blank layout (#12)

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fs = require('fs');
+const loginHTML = fs.readFileSync('./views/login.html','utf8'); 
+const gameHTML = fs.readFileSync('./views/game.html','utf8');
 app.use('/static', express.static('public'));
-
-const data = fs.readFileSync('./views/login.html','utf8'); 
-
-app.get('/', (req, res) => res.send(data))
-
+app.get('/', (req, res) => res.send(loginHTML))
+app.get('/game', (req, res) => res.send(gameHTML))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
