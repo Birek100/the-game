cd99682ecdf819604aee17d8669eac24f0824c4c
Bartosz Szura <bartosz.szura100@gmail.com>
2019-09-25 17:42:04 +0200

    update

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fs = require('fs');
+const loginHTML = fs.readFileSync('./views/login.html','utf8'); 
+const gameHTML = fs.readFileSync('./views/game.html','utf8');
 app.use('/static', express.static('public'));
-
-const data = fs.readFileSync('./views/login.html','utf8'); 
-
-app.get('/', (req, res) => res.send(data))
-
-const data1 = fs.readFileSync('./views/game.html','utf8');
-
-app.get('/game', (req, res) => res.send(data1))
-
+app.get('/', (req, res) => res.send(loginHTML))
+app.get('/game', (req, res) => res.send(gameHTML))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
