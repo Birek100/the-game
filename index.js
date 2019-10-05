740ac2f6cb3cd5b6c11ecf37982f1c1b0df0600f
Bartosz Szura <bartosz.szura100@gmail.com>
2019-10-05 22:14:41 +0200

    test2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 const fs = require('fs');
 const loginHTML = fs.readFileSync('./views/login.html','utf8'); 
 const gameHTML = fs.readFileSync('./views/game.html','utf8');
@@ -11,8 +11,8 @@ app.get('/', (req, res) => res.send(loginHTML))
 app.get('/game', (req, res) => res.send(gameHTML))
 app.get('/register', (req, res) => res.send(registerHTML))
 app.post('/register', (req, res) => {
+  console.log('--------')
   console.log(req.body)
   res.end()
 })
-const data = fs.writeFileSync('./data.txt', 'proba')
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
