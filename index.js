673c518e5e1b4c7a7d95643c852c2ed46a6e8a42
Bartosz Szura <bartosz.szura100@gmail.com>
2019-10-16 20:07:17 +0200

    add writing file function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 3000
 const fs = require('fs');
 const loginHTML = fs.readFileSync('./views/login.html','utf8'); 
 const gameHTML = fs.readFileSync('./views/game.html','utf8');
@@ -11,8 +11,10 @@ app.get('/', (req, res) => res.send(loginHTML))
 app.get('/game', (req, res) => res.send(gameHTML))
 app.get('/register', (req, res) => res.send(registerHTML))
 app.post('/register', (req, res) => {
-  console.log('--------')
   console.log(req.body)
-  res.end()
+  const email = req.body.email
+  const password = req.body.password
+  fs.appendFileSync('message.txt', email + ':' + password +'\n', {'flags': 'a+'});
+  res.end() 
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
