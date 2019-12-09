a5c8bdd2f3e7edcb413093c94aeceb9f3adb1d07
Bartosz Szura <bartosz.szura100@gmail.com>
2019-12-09 20:08:15 +0100

    updating game

--- index.js
+++ index.js
@@ -5,16 +5,50 @@ const fs = require('fs');
 const loginHTML = fs.readFileSync('./views/login.html','utf8'); 
 const gameHTML = fs.readFileSync('./views/game.html','utf8');
 const registerHTML = fs.readFileSync('./views/register.html','utf8');
-app.use(express.urlencoded())
+const errloginHTML = fs.readFileSync('./views/errlogin.html', 'utf8');
+app.use(express.urlencoded());
 app.use('/static', express.static('public'));
 app.get('/', (req, res) => res.send(loginHTML))
 app.get('/game', (req, res) => res.send(gameHTML))
 app.get('/register', (req, res) => res.send(registerHTML))
+app.get('/errlogin', (req, res) => res.send(errloginHTML))
 app.post('/register', (req, res) => {
-  console.log(req.body)
   const email = req.body.email
   const password = req.body.password
-  fs.appendFileSync('users.db', email + ':' + password +'\n', {'flags': 'a+'});
-  res.end() 
-})
+  fs.appendFileSync('users.db', email + ':' + password +'\n', {flags: 'a+'});
+  res.end()
+});
+
+app.get('/login-auth', (req, res) => {
+	const name = req.query.name;
+	const pass = req.query.password;
+
+	console.log(req.query.name)
+	console.log(req.query.password)
+	const registeredUsers = fs.readFileSync('users.db').toString().split("\n");
+	console.log(registeredUsers)
+
+	for(let i = 0; i < registeredUsers.length; i++)
+	{
+		const user = registeredUsers[i];
+		const n = user.split(':')[0];
+		const p = user.split(':')[1];
+		
+
+		if (n === name && p === pass) {
+			res.cookie('sid', n + p, { maxAge: 900000, httpOnly: true });
+			res.send(gameHTML);
+			return;
+		}
+		else {
+			res.send(errloginHTML);
+			return;
+		}
+
+	}
+
+	// SET COOKIE
+});
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
