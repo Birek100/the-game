5dd82cdff77d2bd7476155d34e041abc2cdc58f3
Bartosz Szura <bartosz.szura100@gmail.com>
2019-12-10 05:24:18 +0100

    next update

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fs = require('fs');
+
 const loginHTML = fs.readFileSync('./views/login.html','utf8'); 
 const gameHTML = fs.readFileSync('./views/game.html','utf8');
 const registerHTML = fs.readFileSync('./views/register.html','utf8');
 const errloginHTML = fs.readFileSync('./views/errlogin.html', 'utf8');
+
 app.use(express.urlencoded());
 app.use('/static', express.static('public'));
 app.get('/', (req, res) => res.send(loginHTML))
 app.get('/game', (req, res) => res.send(gameHTML))
 app.get('/register', (req, res) => res.send(registerHTML))
 app.get('/errlogin', (req, res) => res.send(errloginHTML))
+
 app.post('/register', (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
-  fs.appendFileSync('users.db', email + ':' + password +'\n', {flags: 'a+'});
-  res.end()
+ 	const email = req.body.email
+ 	const password = req.body.password
+ 	fs.appendFileSync('users.db', email + ':' + password +'\n', {flags: 'a+'});
+ 	res.end()
 });
 
 app.get('/login-auth', (req, res) => {
@@ -40,15 +43,12 @@ app.get('/login-auth', (req, res) => {
 			res.send(gameHTML);
 			return;
 		}
+		
 		else {
 			res.send(errloginHTML);
 			return;
 		}
-
 	}
-
-	// SET COOKIE
 });
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
