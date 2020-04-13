d8d2e14a342f36054f11284b6ee7480fa9bfff6a
Bartosz Szura <bartosz.szura100@gmail.com>
2020-04-13 19:25:28 +0200

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     ecmaVersion: 2018
   },
   rules: {
-    //'linebreak-style': ['error', 'windows'],
+    'linebreak-style': ['error', 'windows'],
     'comma-dangle': ['error', 'never']
   }
 };
