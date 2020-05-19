5572c067e9b42adc178af746e3397ee6186114ca
Bartosz Szura <bartosz.szura100@gmail.com>
2020-05-19 14:26:33 +0200

    ready first app

--- client/app.jsx
+++ client/app.jsx
@@ -1,34 +1,29 @@
 import React, { Component } from 'react';
 
-let calc = 0
-
 class App extends Component {
 	state ={
 		number: 0
 	}
 
 	getLowerNumber = () => {
-		console.log(calc -= 1)
-		calc -= 1
-		//console.log(this.state['number'] -= 1)
-		//	this.state['number'] -= 1 
+		console.log(this.state.number)
+		this.setState({number: this.state.number - 1})
 	};
-
 	getHigherNumber = () => {
-		console.log(calc += 1)
-		calc += 1
+		console.log(this.state.number)
+		this.setState({number: this.state.number + 1})
 	};
 
-	render(){
-		console.log(this.state['number'])
+	render() {
 		return (
 			<div className="App">
 				<button onClick={this.getHigherNumber}>Plus +</button>
 				<button onClick={this.getLowerNumber}>Minus -</button>
-				<p> Your actual number is { calc } </p>
+				<p> Your actual state is { this.state['number'] } </p>
 			</div>
 			);
 	}
+	
 }
 
 export default App;
