25f020b634305b5b4380a8dce3fba3ed9ee9cd17
Birek100 <48009602+Birek100@users.noreply.github.com>
2020-06-16 17:09:17 +0200

    GAME-29 - Move react/redux application into game.html (#28)

--- client/container.jsx
+++ client/container.jsx
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { increase, decrease } from './action';
-import App from './app.jsx'
+import App from './app.jsx';
 
 const mapStateToProps = state => {
   return {
