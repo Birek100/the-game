b4ac4a1c39a018e31b82835ade2db0b893c072b7
Bartosz Szura <bartosz.szura100@gmail.com>
2020-11-20 10:45:06 +0100

    update

--- client/components/game/game.jsx
+++ client/components/game/game.jsx
@@ -2,42 +2,30 @@ import React, {useRef, useState, useEffect} from 'react';
 
 function Game () {
   const canvasRef = useRef(null);
-  const [penguinx, setPenguinx] = useState(50);
-  const [penguiny, setPenguiny] = useState(250);
+  let [penguinx, setPenguinx] = useState(50);
+  let [penguiny, setPenguiny] = useState(250);
   console.log(canvasRef);
-  
-let penguin = (ctx) => {
+  let move = () => {setPenguinx(prevpenguinx => prevpenguinx + 1); console.log(penguinx)};
+  let penguin = (ctx) => {
     ctx.fillStyle = "#FF0000";
-    ctx.fillRect(penguinx,250,20,20);
-  }
-function update () {
-  const canvas = canvasRef.current;
-  const ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, 500, 500);
-  setPenguinx(prevpenguinx => prevpenguinx + 1);
-  console.log(penguinx);
-  requestAnimationFrame(update)
-}
-
-  function start (ctx) {
-    const canvas = canvasRef.current;
-    penguin(ctx);
-    requestAnimationFrame(update)
+    ctx.fillRect(penguinx,penguiny,20,20);
   }
 
   useEffect(() => { 
-    const canvas = canvasRef.current;
-    console.log(canvas);
-    console.log(canvasRef);
+    const render = () => {
+    move();
+    requestAnimationFrame(render);
+    }
+     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-    start(ctx);
-   
-    //let interval = setInterval(loop, 30);
-  }, [update])
+    penguin(ctx);
+    render();
+  }, []);
 
   return (
       <div>
       <h1>GAME</h1>
+      <button onClick={move}>Move</button>
       <canvas ref={canvasRef} width={500} height={500} />
       </div>
     )
