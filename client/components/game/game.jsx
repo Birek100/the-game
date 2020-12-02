ec21515394038449e37ad3ec9e090c976469f70d
Bartosz Szura <bartosz.szura100@gmail.com>
2020-12-02 09:01:50 +0100

    add moving

--- client/components/game/game.jsx
+++ client/components/game/game.jsx
@@ -5,20 +5,76 @@ const penguin = (ctx, penguinx, penguiny) => {
   ctx.fillRect(penguinx, penguiny, 20, 20);
 }
 
+
+
 function Game () {
   const canvasRef = useRef(null);
-  let [penguinx, setPenguinx] = useState(50);
-  let [penguiny, setPenguiny] = useState(250);
+  let [penguinx, setPenguinx] = useState(240);
+  let [penguiny, setPenguiny] = useState(450);
+
+  const penguinRight = () => {
+  setPenguinx(prevpenguinx => ++prevpenguinx);
+}
+
+const penguinLeft = () => {
+  setPenguinx(prevpenguinx => --prevpenguinx);
+}
+
+document.onkeyup = function(e) {
+  if(e.keyCode == 37) { penguinLeft() }
+  if(e.keyCode == 39) { penguinRight() }
+}
+
+function obstacle(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;    
+    this.draw = function(ctx){
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+  
+/*
+ useEffect(() => {
+  const canvas = canvasRef.current;
+       const ctx = canvas.getContext('2d');
+       penguin(ctx, penguinx, penguiny)
+       setTimeout(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    setPenguinx(prevpenguinx => ++prevpenguinx);
+    console.log(penguinx)
+    }, 1000, ctx) 
+    }, [penguinx, penguiny]);
+*/
+
+//setTimeout((ctx) => {
+//    setPenguinx(prevpenguinx => ++prevpenguinx);
+//    ctx.clearRect(0, 0, canvas.width, canvas.height)
+//    }, 1000) 
+
+
 
   setTimeout(() => {
-    setPenguinx(prevpenguinx => ++prevpenguinx);  
     const canvas = canvasRef.current;
-
+    const ctx = canvas.getContext('2d');
+    console.log(penguinx)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    penguin(ctx, penguinx, penguiny);
+    let blueGamePiece = new obstacle(75, 75, "blue", 10, 10);
+    blueGamePiece.draw(ctx);
+    //setPenguinx(prevpenguinx => ++prevpenguinx);  
+    
     if (canvas) {
-      const ctx = canvas.getContext('2d');
-      penguin(ctx, penguinx, penguiny);
-    }
-  }, 1000);
+      
+      //penguin(ctx, penguinx, penguiny);
+
+    } 
+  }, 1000, penguinx, penguiny); 
+
+
 
   return (
       <div>
