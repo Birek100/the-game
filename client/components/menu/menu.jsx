8adcf2ccd30cb9be30cdde8f68bb5361ed872a93
Bartosz Szura <bartosz.szura100@gmail.com>
2021-02-01 21:01:00 +0100

    update

--- client/components/menu/menu.jsx
+++ client/components/menu/menu.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Menu() {
   return (
     <div className="menu">
       <div className="menu__buttons">
-        <Link className="menu__link" to="/game">
+        <a className="menu__link" href="/game">
           <button type="button" className="menu__button">
             START GAME
           </button>
-        </Link>
-        <Link className="menu__link" to="/about">
+        </a>
+        <a className="menu__link" href="/about">
           <button type="button" className="menu__button">
             ABOUT
           </button>
-        </Link>
+        </a>
         <a className="menu__link" href="/logout">
           <button type="button" className="menu__button">
             LOGOUT
